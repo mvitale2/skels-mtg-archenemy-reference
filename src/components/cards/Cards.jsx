7d81f1884e090bb891e4e5f04e3f4c9f2328995e
Skel <122138703+mvitale2@users.noreply.github.com>
2024-10-30 13:02:57 -0400

    the filters work (but they're inversed)

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import CardSkel from "./CardSkel.jsx"
 import "./Cards.css"
 
-function Cards() {
+function Cards({ oarc, oe01, dsc }) {
   return (
     <div className="gallery">
-      <CardSkel set_code="oarc"/>
-      <CardSkel set_code="oe01"/>
-      <CardSkel set_code="dsc"/>
+      {oarc ? <CardSkel set_code="oarc"/> : null}
+      {oe01 ? <CardSkel set_code="oe01"/> : null}
+      {dsc ? <CardSkel set_code="dsc"/> : null}
     </div>
   );
 }
--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -1,28 +1,95 @@
-import React from 'react'
-import "./Filters.css"
+import React, { useState } from "react";
+import "./Filters.css";
 
-function Filters() {
+function Filters({ onFilterSubmit }) {
+  const [useOarc, setOarc] = useState(true)
+  const [useOe01, setOe01] = useState(true);
+  const [useDsc, setDsc] = useState(true);
+  const [useReprints, setReprints] = useState(true);
+
+  const handleFilterChange = (e) => {
+    const { id, checked } = e.target;
+    console.log(`${id} is ${checked}`)
+    if (id == "oarc") {
+      setOarc(!useOarc)
+      console.log(`oarc is now ${useOarc}`)
+    } else if (id === "oe01") {
+      setOe01(!useOe01)
+      console.log(`oe01 is now ${useOe01}`)
+    } else if (id === "dsc") {
+      setDsc(!useDsc)
+      console.log(`dsc is now ${useDsc}`)
+    } else if (id === "reprints") {
+      setReprints(!useReprints)
+      console.log(`reprints is now ${useReprints}`)
+    }
+  }
+  
   return (
     <div className="filters">
-      <form name="filters-form">
-        <input type="checkbox" name="oarc"></input>
-        <label htmlFor="oarc" id="oarc-chkbox" name="oarc-chkbox">
-          Enable Original Schemes
-        </label>
-        <input type="checkbox" name="oe01"></input>
-        <label htmlFor="oe01" id="oe01-chkbox" name="oe01-chkbox">
-          Enable Nicol Bolas Schemes
-        </label>
-        <input type="checkbox" name="dsc"></input>
-        <label htmlFor="dsc" id="dsc-chkbox" name="dsc-chkbox">
-          Enable Duskmourn Schemes
-        </label>
-        <input type="checkbox" name="reprints"></input>
-        <label>Allow reprints?</label>
-        <button type='submit'>Apply</button>
+      <form
+        className="filters-form"
+        name="filters-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+        }}
+      >
+        <div className="selections">
+          <div className="filter">
+            <input
+              className="chkbox"
+              type="checkbox"
+              id="oarc"
+              name="oarc-chkbox"
+              checked={useOarc}
+              onChange={handleFilterChange}
+            ></input>
+            <label htmlFor="oarc-chkbox">Enable Original Schemes</label>
+          </div>
+          <div className="filter">
+            <input
+              className="chkbox"
+              type="checkbox"
+              name="oe01-chkbox"
+              id="oe01"
+              checked={useOe01}
+              onChange={handleFilterChange}
+            ></input>
+            <label htmlFor="oe01-chkbox">Enable Nicol Bolas Schemes</label>
+          </div>
+          <div className="filter">
+            <input
+              className="chkbox"
+              type="checkbox"
+              name="dsc-chkbox"
+              id="dsc"
+              checked={useDsc}
+              onChange={handleFilterChange}
+            ></input>
+            <label htmlFor="dsc-chkbox">Enable Duskmourn Schemes</label>
+          </div>
+          <div className="filter">
+            <input
+              className="chkbox"
+              type="checkbox"
+              id="reprints"
+              name="reprints-chkbox"
+              onChange={handleFilterChange}
+              checked={useReprints}
+            ></input>
+            <label htmlFor="reprints">Allow reprints?</label>
+          </div>
+        </div>
+        <div className="buttons">
+          <button className="submit-btn" type="submit" onSubmit={onFilterSubmit}>
+            Apply Filters
+          </button>
+          <button className="deck-btn">Create Scheme Deck From Filters</button>
+        </div>
       </form>
     </div>
   );
 }
 
-export default Filters
+export default Filters;
